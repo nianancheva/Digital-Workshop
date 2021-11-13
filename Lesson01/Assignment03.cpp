6182a07d7777a0f77a2a80d1c81f38fb6af1b1fb
Nia Nancheva <niaplnan.personal@gmail.com>
2021-11-13 17:01:59 +0200

    Create Assignment03.cpp

---
+++ Lesson01/Assignment03.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  
+    /*
+    Write a program that calculates the perimeter and face of a rectangle, 
+    in which you receive the length and the width of the rectangle as user input. 
+    Use cin and cout.
+    */
+
+    double x;
+    double y;
+
+    cin >> x;
+    cin >> y;
+
+    double rectArea = x * y;
+    double rectPerimeter = 2 * (x + y);
+
+    cout << rectArea << endl<< rectPerimeter;
+
+    //return 0;
+}
