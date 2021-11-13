450b1f138c022e8cbee1d15185da30bfb930f483
Nia Nancheva <niaplnan.personal@gmail.com>
2021-11-13 17:16:02 +0200

    Update Assignment02.cpp

--- Lesson01/Assignment02.cpp
+++ Lesson01/Assignment02.cpp
@@ -8,7 +8,7 @@ int main()
     Write a program that calculates the perimeter and area of a rectangle with length and width 5.4 and 7.9.
     */
 
-    double x = 5.4;
+    double x = 5.4;   //double x = 5.4, y = 7.9;
     double y = 7.9;
 
     double rectArea = x * y;
