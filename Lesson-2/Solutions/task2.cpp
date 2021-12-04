bcf5ca65b90534e1c8f4c4232c3074d1a8fcc113
Nia Nancheva <niaplnan.personal@gmail.com>
2021-12-04 09:16:05 +0200

    Update task2.cpp

--- Lesson-2/Solutions/task2.cpp
+++ Lesson-2/Solutions/task2.cpp
@@ -11,8 +11,14 @@ int main() {
 	if (n < 100) {
 		result = "Less than 100";
 	}
+	else if (n == 100) {
+		result = "Equal to 100";
+	}
 	else if (n > 200) {
 		result = "Greater than 200";
+	}
+	else if (n == 200) {
+		result = "Equal to 200";
 	}
 	else {
 		result = "Between 100 and 200";
