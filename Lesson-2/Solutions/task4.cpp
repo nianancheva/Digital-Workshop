83a9d1e01996534e4f41ec1f8a5be8840543d4a0
Nia Nancheva <niaplnan.personal@gmail.com>
2021-12-04 09:57:28 +0200

    Update task4.cpp

--- Lesson-2/Solutions/task4.cpp
+++ Lesson-2/Solutions/task4.cpp
@@ -5,50 +5,6 @@
 using namespace std;
 int main() {
 
-	string figureType = ""; //can e replaced with char variable
-				//char figureType;
-
-	cin >> figureType;
-
-	double area = 0;
-
-	if (figureType == "s") {
-		double aS;
-		cin >> aS;
-		area = aS * aS;
-	}
-	else if (figureType == "r") {
-		double aR;
-		double bR;
-		cin >> aR >> bR;
-		area = aR * bR;
-	}
-	else if (figureType == "c") {
-		double r;
-		cin >> r;
-		area = r * r * M_PI;
-	}
-	else {
-		double aT;
-		double hA;
-		cin >> aT >> hA;
-		area = aT * hA / 2;
-	}
-
-	cout << fixed << setprecision(3) << area;
-}
-
-/*
-
-//SOLUTION WITH CHAR
-
-#define _USE_MATH_DEFINES
-#include <cmath>
-#include <iostream>
-#include <iomanip>
-using namespace std;
-int main() {
-
 	char figureType;
 
 	cin >> figureType;
@@ -59,25 +15,28 @@ int main() {
 		double aS;
 		cin >> aS;
 		area = aS * aS;
+		cout << area;
 	}
 	else if (figureType == 'r') {
 		double aR;
 		double bR;
 		cin >> aR >> bR;
 		area = aR * bR;
+		cout << area;
 	}
 	else if (figureType == 'c') {
 		double r;
 		cin >> r;
 		area = r * r * M_PI;
+		cout << fixed << setprecision(3) << area;
 	}
 	else {
 		double aT;
 		double hA;
 		cin >> aT >> hA;
 		area = aT * hA / 2;
+		cout << area;
 	}
 
-	cout << fixed << setprecision(3) << area;
+	//cout << fixed << setprecision(3) << area;
 }
-*/
