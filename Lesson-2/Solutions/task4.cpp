92d36bb3e4a9f407acb57748ca1854a7780ab8b8
Nia Nancheva <niaplnan.personal@gmail.com>
2021-12-04 09:33:14 +0200

    task4.cpp --> solution with char

--- Lesson-2/Solutions/task4.cpp
+++ Lesson-2/Solutions/task4.cpp
@@ -5,7 +5,8 @@
 using namespace std;
 int main() {
 
-	string figureType = "";
+	string figureType = ""; //can e replaced with char variable
+				//char figureType;
 
 	cin >> figureType;
 
@@ -36,3 +37,47 @@ int main() {
 
 	cout << fixed << setprecision(3) << area;
 }
+
+/*
+
+//SOLUTION WITH CHAR
+
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+using namespace std;
+int main() {
+
+	char figureType;
+
+	cin >> figureType;
+
+	double area = 0;
+
+	if (figureType == 's') {
+		double aS;
+		cin >> aS;
+		area = aS * aS;
+	}
+	else if (figureType == 'r') {
+		double aR;
+		double bR;
+		cin >> aR >> bR;
+		area = aR * bR;
+	}
+	else if (figureType == 'c') {
+		double r;
+		cin >> r;
+		area = r * r * M_PI;
+	}
+	else {
+		double aT;
+		double hA;
+		cin >> aT >> hA;
+		area = aT * hA / 2;
+	}
+
+	cout << fixed << setprecision(3) << area;
+}
+*/
