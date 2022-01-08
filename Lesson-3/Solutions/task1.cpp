f3df1b6298d21f2aa826eeb90edea4b6a53c22b0
Justsvetoslavov <50053338+Justsvetoslavov@users.noreply.github.com>
2022-01-08 17:55:41 +0200

    Update task1.cpp

--- Lesson-3/Solutions/task1.cpp
+++ Lesson-3/Solutions/task1.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-long long number, sum = 0;
-
-do
-{
+	long long number, sum = 0;
+	
+	do
+	{
 	cin >> number;
 	sum += number;
-}while(number != 0);
-
-cout << "Sum is: " << sum << endl;
+	}while(number != 0);
+	
+	cout << "Sum is: " << sum << endl;
 }
