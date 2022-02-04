51eea32f636c623a94a00bfbcdc18878630e54e0
Nia Nancheva <niaplnan.business@gmail.com>
2022-02-04 20:27:11 +0200

    add task 1 demo solution

---
+++ Homework-1/Solutions/task1.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+using namespace std;
+
+int greaterNumber(int a, int b) {
+	if (a > b)
+		return a;
+	else 
+		return b;
+}
+
+int main() {
+
+	long long input;
+
+	cin >> input;
+
+	long long number = input;
+
+	long long numL = input, numR = input;
+
+	//--------
+
+	long long numberOfDigits = 0;
+
+	while (input != 0) {
+		input /= 10;
+		numberOfDigits++;
+	}
+
+	//--------
+	
+	long long pL = 1;
+
+	for (int i = 1; i < numberOfDigits; i++) {
+		pL *= 10;
+	}
+
+	for (int i = 0; i < numberOfDigits / 2; i++) {
+		int nL = numL / pL;
+		int nR = numR % 10;
+
+		if (nL == nR) {
+			cout << 0;
+		}
+
+		if (i == numberOfDigits / 2 - 1) {
+			cout << greaterNumber(nL, nR);
+		}
+		else { cout << greaterNumber(nL, nR)<< " "; }
+		
+		numL = numL - nL * pL;
+		numR = numR / 10;
+		pL /= 10;
+		//cout << nL << " " << nR << endl;
+	}
+
+	if (numberOfDigits % 2 != 0) {
+		if (numberOfDigits == 1) {
+				cout << numR % 10;
+		}
+		else {
+			cout << " " << numR % 10;
+		}
+	}
+
+	if (number == 0)
+		cout << 0;
+
+}
