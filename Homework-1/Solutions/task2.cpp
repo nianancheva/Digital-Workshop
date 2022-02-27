ec636cd52273024a698ad71d21886fc4cd8217b2
Nia Nancheva <niaplnan.business@gmail.com>
2022-02-27 09:26:47 +0200

    task2 - updated

--- Homework-1/Solutions/task2.cpp
+++ Homework-1/Solutions/task2.cpp
@@ -1,26 +1,36 @@
 #include <iostream>
 using namespace std;
 
+bool isNatural(double n) {
+	long long natural = n;
+	return n == natural;
+}
+
 int main() {
 
-	long long input;
-
+	double input;
 	cin >> input;
 
 	long long number = input;
+	
+	if (!isNatural(input)) {
+			cout << "Number must be natural!";
+			return 0;
+		}
 
-	long long p = 1;
+	long long multiplier = 1;
 	long long result = 0;
 
 	while (number != 0) {
-		int n = number % 10;
 
-		if (n % 2 != 0) {
-			result += n * p;
-			p *= 10;
+		int lastDigit = number % 10;
+
+		if (lastDigit % 2 != 0) {
+			result += lastDigit * multiplier;
+			multiplier *= 10;
 		}
 
-		number = number / 10;
+		number /= 10;
 	}
 
 	cout << result;
